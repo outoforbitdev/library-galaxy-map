60c0574dd6082f6e10cd3b239cbe025f3ec7826a
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2025-10-16 08:46:03 -0400

    feat: add map legend (#77)
    * feat: add map legend
    
    * chore: lint
    
    * fix: don't show legend if there are no entries
    
    * chore: lint

--- src/components/MapUI.tsx
+++ src/components/MapUI.tsx
@@ -1,15 +1,20 @@
 import { IComponentProps } from "@outoforbitdev/ood-react";
 import { MapOptions, IMapOptionsProps } from "./MapOptions";
+import { LegendEntry, MapLegend } from "./MapLegend";
 import styles from "../styles/map.module.css";
 
 interface IMapUIProps extends IComponentProps {
   mapOptions: IMapOptionsProps;
   customOptions?: React.ReactNode;
+  legendEntries?: LegendEntry[];
 }
 
 export function MapUI(props: IMapUIProps) {
   return (
     <div className={styles.mapOverlay}>
+      {props.legendEntries && props.legendEntries.length > 0 ? (
+        <MapLegend entries={props.legendEntries} />
+      ) : null}
       <MapOptions {...props.mapOptions}>{props.customOptions}</MapOptions>
       {props.children}
     </div>
