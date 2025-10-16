d1acaf7ab60c71d7c5204bdc2107b006045e0dd4
Jay Mirecki <jarett.mirecki@gmail.com>
2025-10-16 08:33:08 -0400

    chore: lint

--- src/components/MapLegend.tsx
+++ src/components/MapLegend.tsx
@@ -5,13 +5,13 @@ import { colorToCss, MapColor } from "./Colors";
 import PlanetMap from "./PlanetMap";
 
 export interface LegendEntry {
-    id: string;
-    label: string;
-    color: MapColor
+  id: string;
+  label: string;
+  color: MapColor;
 }
 
 export interface IMapLegendProps extends IChildlessComponentProps {
-    entries?: LegendEntry[];
+  entries?: LegendEntry[];
 }
 
 export function MapLegend(props: IMapLegendProps) {
@@ -23,7 +23,7 @@ export function MapLegend(props: IMapLegendProps) {
     >
       {props.entries?.map((entry) => (
         <span key={entry.id}>
-            <ExampleMapItem color={entry.color} />
+          <ExampleMapItem color={entry.color} />
           {entry.label}
         </span>
       ))}
@@ -31,12 +31,36 @@ export function MapLegend(props: IMapLegendProps) {
   );
 }
 
-function ExampleMapItem(props: {color: MapColor}) {
-    return (
-        <svg height="1rem" viewBox="0 0 70 30" preserveAspectRatio="xMidYMid meet">
-            <line x1="5" y1="15" x2="5" y2="15" stroke={colorToCss(props.color)} strokeWidth="10px" strokeLinecap="round" />
-            <line x1="5" y1="15" x2="55" y2="15" stroke={colorToCss(props.color)} strokeWidth="5px" strokeLinecap="round" />
-            <line x1="55" y1="15" x2="55" y2="15" stroke={colorToCss(props.color)} strokeWidth="10px" strokeLinecap="round" />
-        </svg>
-    )
+function ExampleMapItem(props: { color: MapColor }) {
+  return (
+    <svg height="1rem" viewBox="0 0 70 30" preserveAspectRatio="xMidYMid meet">
+      <line
+        x1="5"
+        y1="15"
+        x2="5"
+        y2="15"
+        stroke={colorToCss(props.color)}
+        strokeWidth="10px"
+        strokeLinecap="round"
+      />
+      <line
+        x1="5"
+        y1="15"
+        x2="55"
+        y2="15"
+        stroke={colorToCss(props.color)}
+        strokeWidth="5px"
+        strokeLinecap="round"
+      />
+      <line
+        x1="55"
+        y1="15"
+        x2="55"
+        y2="15"
+        stroke={colorToCss(props.color)}
+        strokeWidth="10px"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
 }
